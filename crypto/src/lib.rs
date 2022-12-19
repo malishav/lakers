583612ac7a142830543d8b2fb4dc69a3a7d85ef2
Mališa Vučinić <malishav@gmail.com>
2022-12-19 14:58:22 +0100

    crypto-psa: Implement the CryptoProvider interface with PSA crypto

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -7,4 +7,4 @@ pub use edhoc_crypto_hacspec::HacspecCryptoProvider as CryptoProvider;
 pub use edhoc_crypto_cc2538::Cc2538CryptoProvider as CryptoProvider;
 
 #[cfg(feature = "psa")]
-pub use edhoc_crypto_psa::*;
+pub use edhoc_crypto_psa::PsaCryptoProvider as CryptoProvider;
